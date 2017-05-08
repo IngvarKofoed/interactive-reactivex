b8db6009b7ffc8260f5a5fb67ea46f33ea143879
Martin Ingvar Kofoed Jensen <MJE@foss.dk>
2017-05-08 15:17:35 +0200

    Adding handling of static resources

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: "./src/svg-render.ts",
   output: {
     filename: "./dist/svg-render.js"
   },
   resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: [".ts"]
   },
   module: {
     loaders: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
       { test: /\.ts$/, loader: "ts-loader" }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { context: './static', from: "**.*", to: "./dist" }
+    ])
+  ]
 }
